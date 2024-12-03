a080853e2a5e21187e26ac57849dbe5608548f17
luizffdemoraes <lffm1994@gmail.com>
2024-12-02 22:35:52 -0300

    Projeto base.

---
+++ src/main/java/com/devsuperior/eventacademy/EventacademyApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.eventacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventacademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/eventacademy/EventacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.eventacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
